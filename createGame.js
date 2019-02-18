c58974c73d052575eaa1bf75716980d3a1e7cc8a
Andre Rivie <andre.rivie@gmail.com>
2019-02-17 23:13:38 -0500

    randomize tiles on create, added comments, small bug fixes

--- createGame.js
+++ createGame.js
@@ -1,9 +1,28 @@
+let numberArray = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+
+// shuffle method defined below
+shuffleArray(numberArray)
+
 const blockContainer = document.getElementById("app")
 
-for (i=1; i<=15; i++) {
+// create the starting board html elements
+for (i = 0; i <= 14; i++) {
   const block = document.createElement("div")
   block.className = "block-item"
-  block.id = i
-  block.innerHTML = i
+  // keep the html ids 1-indexed
+  block.id = i + 1
+  const blockNumber = document.createElement("h1")
+  const blockNumberText = document.createTextNode(`${numberArray[i]}`)
+  blockNumber.appendChild(blockNumberText)
+  block.appendChild(blockNumber)
   blockContainer.appendChild(block)
+}
+
+// nice ES6 durstenfield shuffle implementation copied from stackoverflow
+// https://stackoverflow.com/a/12646864
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+  }
 }
--- handleClick.js
+++ handleClick.js
@@ -1,58 +1,72 @@
+// global space variable is initialized to 16 and changes with valid click
 let space = 16
+
+// helper object used for style.transform and html id reassignment once a move
+// direction has been determined
 const move = {
   right: [100, 0],
   rightAdjust: 1,
   left: [-100, 0],
   leftAdjust: -1,
   down: [0, 100],
-  downAdjust: +4,
+  downAdjust: 4,
   up: [0, -100],
   upAdjust: -4
 }
 
 document.addEventListener('click', (evt) => {
-  const blockClicked = evt.target
+  let blockClicked = evt.target
+  // adjust blockClicked to parent if text is clicked
+  if (!blockClicked.className && blockClicked.parentElement) {
+    blockClicked = blockClicked.parentElement
+  }
+  // uses helper function explained below
   const options = getOptions(blockClicked.id)
   let directionToMove = ''
-
   for (direction in options) {
+    // determine which direction the space is in based on move options
     if (options[direction].includes(space)) {
       directionToMove = direction
     }
   }
-
+  // if directionToMove is truthy, there's a move to be made
+  // otherwise, just console.log('invalid move')
   if (directionToMove) {
+    // get array of blocks we need to move based on determined direction
     let blocksToMove = options[directionToMove]
+    // different rules for right/down and left/up due to order
     if (directionToMove === 'right' || directionToMove === 'down') {
       blocksToMove.reverse().push(parseInt(blockClicked.id))
       blocksToMove.shift()
+      // remove options we don't want to move
       blocksToMove = blocksToMove.filter(ele => ele < space)
     } else {
+      // add clicked block id to beginning of array then pop off ending
       blocksToMove.unshift(parseInt(blockClicked.id))
       blocksToMove.pop()
-      blocksToMove.sort()
+      // remove options we don't want to move
       blocksToMove = blocksToMove.filter(ele => ele > space)
+      blocksToMove = blocksToMove.sort((a,b) => a-b)
     }
+    // set the new space variable to what was clicked
     space = parseInt(blockClicked.id)
+    // loop through blocksToMove array and:
+    // 1. set a new style.transform based on move direction/current transform value
+    // 2. reassign the block's id based on move direction
     for (blockId in blocksToMove) {
       currentBlockId = blocksToMove[blockId]
       const currentBlock = document.getElementById(currentBlockId)
       const newRule = setCurrentTranslate(currentBlock.style.transform, move[directionToMove])
       currentBlock.style.transform = newRule
       currentBlock.id = currentBlockId + move[`${directionToMove}Adjust`]
-      // currentBlock.style.transform = 'none'
     }
   } else {
     console.log('Invalid move!')
   }
-
-
-
-
-
-  // block.style.transform = moveUp
 })
 
+// gets all move options from any square and returns them in the form of an
+// objtions object with arrays for up, down, left, right
 const getOptions = (id) => {
   const blockId = parseInt(id)
   let options = {
@@ -84,18 +98,15 @@ const getOptions = (id) => {
   return options
 }
 
+// helper for grabbing current translate values and adjusting them based on click
 const setCurrentTranslate = (currentRule, xyArray) => {
   if (currentRule) {
-    console.log(currentRule)
+    // regex will return array of current [x,y] translate values
     let coords = currentRule.match(/[-\d]+/g)
-    console.log(coords)
-
-  console.log(`translate(${parseInt(coords[0]) + parseInt(xyArray[0])}px, ${parseInt(coords[1]) + parseInt(xyArray[1])}px)`)
-  return `translate(${parseInt(coords[0]) + parseInt(xyArray[0])}px, ${parseInt(coords[1]) + parseInt(xyArray[1])}px)`
+    // add current values to xyArray (determined by move[right], move[up], etc)
+    return `translate(${parseInt(coords[0]) + parseInt(xyArray[0])}px, ${parseInt(coords[1]) + parseInt(xyArray[1])}px)`
   } else {
+    // if no current translate values, just set them
     return `translate(${xyArray[0]}px, ${xyArray[1]}px)`
   }
-
 }
-
-// "translate(0px, 100px)"
